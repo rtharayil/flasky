51fdb9a3bb4a93244561f94ba9d187bdb02a235d
rtharayil <ranjiththarayil@gmail.com>
2022-07-13 13:09:22 +0530

    fixed defect ABC , reason XYZ

--- config.py
+++ config.py
@@ -2,6 +2,7 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 
+
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
     MAIL_SERVER = os.environ.get('MAIL_SERVER', 'smtp.googlemail.com')
